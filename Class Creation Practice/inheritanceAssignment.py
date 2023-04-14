0ef7a277acc83d139d5782989880583000e91e2c
daylilyk <105621328+daylilyk@users.noreply.github.com>
2023-04-13 22:31:33 -0700

    Update inheritanceAssignment.py

--- Class Creation Practice/inheritanceAssignment.py
+++ Class Creation Practice/inheritanceAssignment.py
@@ -1,5 +1,4 @@
 
-
 # Parent class
 
 class Artist:
@@ -17,25 +16,42 @@ class Artist:
             print("The password or email is incorrect.")
 
 #child class
-class SocialMedia(Artist):
-    platform = "Instagram"
+class Instagram(Artist):
+    username = "Loren_Nero"
     followers = 10000
     auth = "helloW0rld!"
 
     def getLoginInfo(self):
-        entry_name = input("Enter your name: ")
+        entry_username = input("Enter your username: ")
         entry_email = input("Enter your email: ")
         entry_auth = input("Enter Instagram Password: ")
-        if (entry_email == self.email and entry_auth == self.auth):
-            print("Welcome back, {}! Happy posting!".format(entry_name))
+        if (entry_username == self.username and entry_auth == self.auth):
+            print("Welcome back, {}! Let's sign you into your Facebook.".format(entry_username))
         else:
-            print("The password or email is incorrect.")
+            print("The password or username is incorrect.")
+
+#child class
+class Facebook(Artist):
+    username = "Loren Nero"
+    friends = 351
+    auth = "helloF4cebook!"
+
+    def getLoginInfo(self):
+        entry_username = input("Enter your first and last name: ")
+        entry_email = input("Enter your email: ")
+        entry_auth = input("Enter Facebook Password: ")
+        if (entry_username == self.username and entry_auth == self.auth):
+            print("Welcome back, {}! Happy posting!".format(entry_username))
+        else:
+            print("The password or username is incorrect.")
 
 #Invoking the methods inside each class for Artist and SocialMedia
 
 customer = Artist()
 customer.getLoginInfo()
 
-customerPage = SocialMedia()
+customerPage = Instagram()
 customerPage.getLoginInfo()
-    
+
+customerFB = Facebook()
+customerFB.getLoginInfo()
