7d5852996d01679a93ad66dc53239b74dd762e01
daylilyk <105621328+daylilyk@users.noreply.github.com>
2023-04-14 16:51:27 -0700

    Updating Child Class 2 properties
    Changing individual properties in Facebook class while updating input and print commands in both child classes

--- Class Creation Practice/inheritanceAssignment.py
+++ Class Creation Practice/inheritanceAssignment.py
@@ -1,4 +1,5 @@
 
+
 # Parent class
 
 class Artist:
@@ -11,39 +12,43 @@ class Artist:
         entry_email = input("Enter your email: ")
         entry_password = input("Enter your password: ")
         if (entry_email == self.email and entry_password == self.password):
-            print("Welcome back, {}! Let's get you signed into your Instagram account.".format(entry_name))
+            print("\nWelcome back, {}! \nLet's get you signed into your Instagram account.".format(entry_name))
         else:
             print("The password or email is incorrect.")
 
-#child class
+child class
 class Instagram(Artist):
     username = "Loren_Nero"
-    followers = 10000
+    email = "loren@gmail.com"
     auth = "helloW0rld!"
 
     def getLoginInfo(self):
-        entry_username = input("Enter your username: ")
-        entry_email = input("Enter your email: ")
+        entry_username = input("\nEnter your username: ")
+        entry_email = input("Please confirm your email: ")
         entry_auth = input("Enter Instagram Password: ")
         if (entry_username == self.username and entry_auth == self.auth):
-            print("Welcome back, {}! Let's sign you into your Facebook.".format(entry_username))
+            print("\nWelcome back, {}!".format(entry_username))
+            print("\nNow, let's sign you into your Facebook.")
         else:
-            print("The password or username is incorrect.")
+            print("\nThe password or username is incorrect.")
 
 #child class
 class Facebook(Artist):
-    username = "Loren Nero"
-    friends = 351
+    username = "LorenNero"
+    phoneNum = "803-966-8696"
     auth = "helloF4cebook!"
+    authPin = "9686"
 
     def getLoginInfo(self):
-        entry_username = input("Enter your first and last name: ")
-        entry_email = input("Enter your email: ")
+        entry_username = input("\nEnter your username: ")
+        entry_phoneNum = input("Enter the phone number associated with your account: ")
         entry_auth = input("Enter Facebook Password: ")
-        if (entry_username == self.username and entry_auth == self.auth):
-            print("Welcome back, {}! Happy posting!".format(entry_username))
+        entry_authPin = input("\nOops! It looks like there's been some suspicious activity on your account. \nTo confirm your identity please input your personalized pin number: ")
+        if (entry_username == self.username and entry_phoneNum == self.phoneNum and entry_auth == self.auth and entry_authPin == self.authPin):
+            print("\nWelcome back, {}! Since your last login, \nseveral attempts to login to your account were made from Portand, Oregon. \nPlease go to settings -> security to confirm if this was you.\
+            \nIf it was not, we suggest you change your password!".format(entry_username))
         else:
-            print("The password or username is incorrect.")
+            print("The phone number, password, or pin is incorrect.")
 
 #Invoking the methods inside each class for Artist and SocialMedia
 
