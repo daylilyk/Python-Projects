c44acda32ae2882eef47103b0f7558a6b80edfc7
Lily Kiousis <105621328+daylilyk@users.noreply.github.com>
2023-05-02 19:21:26 -0700

    Update settings.py

--- DjangoUniversity/Class/settings.py
+++ DjangoUniversity/Class/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-SECRET_KEY = config("SECRET_KEY")  # Replaces the secret key
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-gu#wf*-$q&!*bdr_*6vbvbs-o01@hfi=rh&+gq2nx!(7-f!k0z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
